c1f5024c254a8d4f7e467866b2859059e4e95998
Yegor <egorzaremba@gmail.com>
2023-05-30 12:56:17 +0300

    fix(deps): add react-native

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
-    <React.StrictMode>
       <App />
-    </React.StrictMode>
   </BrowserRouter>,
 
   document.getElementById("root")
