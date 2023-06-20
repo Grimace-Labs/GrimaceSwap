891f60a6f5d6b41b258d45230aabeeaa98549d90
DimDimich-creator <stilys62@gmail.com>
2023-06-20 15:04:27 +0300

    fix(UI): connectWalletPage

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,14 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./Footer.css";
 
 function Footer() {
-
-
   return (
-    <footer className='footer'>
-      <p>
-      Copyright © 2023, GRIMACE DOGE COIN
-      </p>
+    <footer className="footer">
+      <p>Copyright © 2023, GRIMACE DOGE COIN</p>
     </footer>
   );
 }
